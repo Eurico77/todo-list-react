2b68d6a6f8a936207b74ee02eded355f60bbd39c
eurico77 <euricomagalhaes61@gmail.com>
2023-05-06 19:26:42 -0300

    refactor: adjustments of responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,5 +3,8 @@ import { Container, Title } from "./style";
 export function Header() {
   return <Container>
     <Title>Todo list</Title>
+    <p style={{
+    color: '#fff',
+    }}>@Eurico_dev</p>
   </Container>;
 }
