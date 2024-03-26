c9058bee75fd958eb75a56b734e1f664030a4a0e
eurico77 <euricomagalhaes61@gmail.com>
2024-03-25 23:40:52 -0300

    Refactor Card component to include a Button and onRemoveTask prop

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,30 +1,36 @@
+import { Button } from '../Button';
 import { CardTitle, CardWrapper, Checkbox } from './style';
 
-export function Card({ title, completed, onToggleTaskCompletion }) {
+export function Card({
+  title,
+  completed,
+  onToggleTaskCompletion,
+  onRemoveTask,
+}) {
   return (
     <>
       {completed && (
         <CardWrapper completed={completed}>
           <Checkbox
-            type='checkbox'
+            type="checkbox"
             checked={completed}
             onChange={onToggleTaskCompletion}
           />
           <CardTitle completed={completed}>{title}</CardTitle>
+          <Button onRemoveTask={onRemoveTask} />
         </CardWrapper>
       )}
 
       {!completed && (
-       <CardWrapper>
-        <Checkbox
-          type='checkbox'
-          checked={completed}
-          onChange={onToggleTaskCompletion}
-        />
-        <CardTitle>{title}</CardTitle>
-      </CardWrapper>
+        <CardWrapper>
+          <Checkbox
+            type="checkbox"
+            checked={completed}
+            onChange={onToggleTaskCompletion}
+          />
+          <CardTitle>{title}</CardTitle>
+        </CardWrapper>
       )}
     </>
-     
   );
 }
